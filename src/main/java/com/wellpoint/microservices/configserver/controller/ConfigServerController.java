415190efb5c037ee4d5adc7752b6d3794be6bd3d
Asok Payyapilly <asok.payyapilly@ust-global.com>
2017-10-30 18:38:07 +0530

    comit

--- src/main/java/com/wellpoint/microservices/configserver/controller/ConfigServerController.java
+++ src/main/java/com/wellpoint/microservices/configserver/controller/ConfigServerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigServerController {
 	
-	@RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+	@RequestMapping(value = "/health", method = RequestMethod.GET)
 	public String healthCheck()
 
 	{
